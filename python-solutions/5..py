61607eac4b8e17dcf6163eab2d167f8c6c712a13
abhay772 <70549773+abhay772@users.noreply.github.com>
2021-07-03 13:30:30 +0530

    ADD: Solution for 05

---
+++ python-solutions/5..py
@@ -0,0 +1,42 @@
+def longestPalindrome(s: str) -> str:
+    Max=s[0]
+    if len(s)==1 or s==s[::-1]:
+        return s
+
+    for left in range(len(s)):
+        right=left+len(Max)
+
+        while(right<len(s)):
+            
+            if s[left:right+1]==s[left:right+1][::-1]:
+                if len(Max)<len(s[left:right+1]):
+                    Max=s[left:right+1]
+            right+=1
+        #print(s[left:right+1])
+    return Max
+
+## Slower than above
+##class Solution:
+##    def longestPalindrome(self, s: str) -> str:
+##        Max=s[0]
+##        
+##        if len(s)==1 or s==s[::-1]:
+##            return s
+##        left=0
+##        while(left<len(s)):
+##            right=left+len(Max)
+##
+##            while(right<len(s)):
+##                
+##                if s[left:right+1]==s[left:right+1][::-1]:
+##                    if len(Max)<len(s[left:right+1]):
+##                        Max=s[left:right+1]
+##                right+=1
+##            if s[left:right+1]==s[left:right+1][::-1]:   
+##                left=right-1
+##            left+=1
+##            #print(s[left:right+1])
+##        return Max
+#s="raedvmtyxveocfyhluuozodpxlroyjcsfslqmjthsbxhteeinpmnejxxcsyjgugclkehagpemfrnqtrkiropblcqdboztxtsaxqnsktrhzelynbzkxcghhfntrdauyzhzgujhniazijshesigzckgxentepeohcltpydumougjkmgoscchqsryaiamoujnyfpcsbwqtwikedbsjxxtnrpfgeqymwfngixslwlifimdapgzanuqwhwpesaigeoiwoyxzjmxukbsvsjvnjhwdbqzuurfolcngefdpsewrpvwivrsjnttrewkytdvvguatidyemrswpdmeqjrxgfdmcdlrcgiqdkyaaykdqigcrldcmdfgxrjqemdpwsrmeyditaugvvdtykwerttnjsrviwvprwespdfegnclofruuzqbdwhjnvjsvsbkuxmjzxyowioegiasepwhwqunazgpadmifilwlsxignfwmyqegfprntxxjsbdekiwtqwbscpfynjuomaiayrsqhccsogmkjguomudyptlchoepetnexgkczgisehsjizainhjugzhzyuadrtnfhhgcxkzbnylezhrtksnqxastxtzobdqclbporikrtqnrfmepgaheklcgugjyscxxjenmpnieethxbshtjmqlsfscjyorlxpdozouulhyfcoevxytmvdear"
+s="ccd"
+print(longestPalindrome(s))
